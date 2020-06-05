3d88b7b9dea815ca76f8a3e252227a477be97737
mandarinkb <mandarinkb@gmail.com>
2020-06-05 23:19:33 +0700

    test BigC v0.2

--- src/test/java/com/projectfinalwebscrappingbot/BigC.java
+++ src/test/java/com/projectfinalwebscrappingbot/BigC.java
@@ -1,12 +1,22 @@
 package com.projectfinalwebscrappingbot;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
 public class BigC {
+	public List<JSONObject> list = new ArrayList<>();
     public String getCateId(String category) {
     	String cate_id = null ;
         switch(category) 
@@ -67,6 +77,48 @@ public class BigC {
         } 
     	return cate_id;
     }
+    
+    public String bigCApi(String cateId ,String page) {
+    	String elsValue = null;
+    	try {
+    		Unirest.setTimeouts(0, 0);
+    		HttpResponse<String> response = Unirest.post("https://www.bigc.co.th/api/categories/getproductListBycateId?_store=2&stock_id=1")
+    		  .header("Content-Type", "application/json")
+    		  .header("Cookie", "__cfduid=d2a52d4a4656e2822d5fa36c91c494f2c1590843646; __cfruid=2cc422a0cc7785dccdce2bc2e0546577f16d6f00-1591341055")
+    		  .body("{\"cate_id\": \""+cateId+"\","
+    		  		+ "\"ignore_items\": \"\","
+    		  		+ "\"page_no\": "+page+","
+    		  		+ "\"page_size\": 36,"
+    		  		+ "\"selected_categories\": \"\","
+    		  		+ "\"selected_brands\": \"\","
+    		  		+ "\"sort_by\": \"bestsellers:desc\","
+    		  		+ "\"price_from\": \"\","
+    		  		+ "\"price_to\": \"\","
+    		  		+ "\"filter\": [],"
+    		  		+ "\"stock_id\": 1}")
+    		  .asString();
+
+    		elsValue = response.getBody();
+    	}catch (Exception ex) {
+        	System.out.println(ex.getMessage());
+        }
+    	
+    	return elsValue;
+    }
+    
+    public int lastPage(String elasValue) {
+    	int last = 0;
+    	try {
+			JSONObject json = new JSONObject(elasValue);
+			JSONObject result = json.getJSONObject("result");
+			last = result.getInt("lastPage");
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+    	return last;
+    }
     public void getCategory(String url){   
     	JSONObject json = new JSONObject();
     	try {
@@ -77,19 +129,83 @@ public class BigC {
 		            .get();//
             Elements eles = doc.select(".swiper-wrapper");
             for (Element ele : eles.select("a")) {
+            	json = new JSONObject();
             	String category = ele.text();
-            	System.out.println(category);
+            	//get cate_id
+            	String cateId = this.getCateId(category);
+            	json.put("category", category);
+            	json.put("cateId", cateId);
+            	list.add(json);
+            	// push ลง redis 
             }
-            //System.out.println(eles);
     	}catch(Exception e) {
     		System.out.println("error => "+e.getMessage());
     	}
+    }
+    public void getContent(String listStr) {
+    	String baseUrl = "https://www.bigc.co.th/";
+     	try {
+			JSONArray arr = new JSONArray(listStr);
+			for (int i = 0; i < arr.length(); i++) {
+				JSONObject obj = arr.getJSONObject(i);
+				String category = obj.getString("category");
+				String cateId = obj.getString("cateId");
+            	//call bigCapi
+            	String elasValue = this.bigCApi(cateId, "1");
+            	//get last page
+                int lastPage = this.lastPage(elasValue);
+                //ตัด สินค้าบริการส่งด่วน ออกไป
+                if(!category.matches("สินค้าบริการส่งด่วน")) {
+                    // get all page and next page เริ่มตั้งแต่หน้าที่ 1
+                    for(int j = 1; j < lastPage; j++) {
+                    	String bigCValue = this.bigCApi(cateId, Integer.toString(j));
+                    	
+                    	// ดึงข้อมูล
+            			JSONObject json = new JSONObject(bigCValue);
+            			JSONObject result = json.getJSONObject("result");
+            			JSONArray arrItems = result.getJSONArray("items");
+            			for (int k = 0; k < arrItems.length(); k++) {
+            				JSONObject objItems = arrItems.getJSONObject(k);
+            				double price = objItems.getDouble("final_price_incl_tax");
+            				double originalPrice = objItems.getDouble("price_incl_tax");
+
+            				// เก็บเฉพาะที่มีส่วนลด
+            				if(price != originalPrice) {
+                				String image = objItems.getString("image");
+                				String name = objItems.getString("name");
+                				String productUrl = baseUrl + objItems.getString("url_key");
+                				//int discount = objItems.getInt("discount");
+                				double discount = (((originalPrice - price) / originalPrice) * 100);
+    				            DecimalFormat df = new DecimalFormat("#"); // #.# แปลงทศนิยม 1 ตำแหน่ง
+    				            discount = Double.parseDouble(df.format(discount));
+                				
+    				            System.out.println(image);//
+    				            System.out.println(name);//
+    				            System.out.println(category);//
+    				            System.out.println(productUrl);//
+    				            //System.out.println(this.icon);
+    				            System.out.println(price);//
+    				            System.out.println(originalPrice);
+    				            System.out.println(discount);
+    				            System.out.println();
+            				}
+            			}
+                    }	
+                }
+				
+			}
+    		
+    	}catch(Exception e) {
+    		System.out.println(e.getMessage());
+    	}
     	
     }
+    
     public static void main(String[] args) {
     	String url = "https://www.bigc.co.th/";
     	BigC b = new BigC();
     	b.getCategory(url);
+    	b.getContent(b.list.toString());
     	
     }
     
